56bf4ee19638033878838513a6d836fabc5f5d8d
Michael Ge <michaelge@wustl.edu>
2023-02-24 15:01:43 -0600

    testing relative root url

--- vite.config.js
+++ vite.config.js
@@ -11,25 +11,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       chunkFileNames: "assets/js/index-45ce05b1.js",
-
-  //       assetFileNames: ({ name }) => {
-  //         if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
-  //           return "assets/[name]-[hash][extname]";
-  //         }
-
-  //         if (/\.css$/.test(name ?? "")) {
-  //           return "assets/css/[name]-[hash][extname]";
-  //         }
-
-  //         // default value
-  //         // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-  //         return "assets/[name]-[hash][extname]";
-  //       },
-  //     },
-  //   },
-  // },
 });
