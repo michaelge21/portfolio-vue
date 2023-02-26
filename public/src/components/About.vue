d51e39a8a266c26a5aba7324379d751da69ddfd1
Michael Ge <54512113+michaelge21@users.noreply.github.com>
2023-02-26 10:57:12 -0600

    trying public move

---
+++ public/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavBar></NavBar>
+</template>
+
+<script>
+import NavBar from "./NavBar.vue";
+
+export default {
+  name: "About",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
