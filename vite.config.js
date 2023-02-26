5f15e96c7f5ff13c5055b2133c1b35b9eabce32c
Michael Ge <54512113+michaelge21@users.noreply.github.com>
2023-02-26 10:39:10 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   // used to ensure that github pages actually loads the damn thing
   base:'/portfolio-vue/',
   plugins: [vue()],
