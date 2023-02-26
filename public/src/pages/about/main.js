0b7e359c6e443d3a911a4afd72f60b3a5ebf623c
Michael Ge <54512113+michaelge21@users.noreply.github.com>
2023-02-26 10:48:51 -0600

    new file struc

--- public/src/pages/about/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import About from "./About.vue";
-
-import "../../assets/stylesheets/main.css";
-
-createApp(About).mount("#app");
--- public/src/pages/portfolio/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./Portfolio.vue";
-
-import "../../assets/stylesheets/main.css";
-
-createApp(App).mount("#app");
